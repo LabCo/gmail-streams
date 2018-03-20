32f205f22f5131de13c5c33db7ecee8983c1e723
Michael Charkin <michael@lablablab.com>
2018-03-19 17:34:10 -0700

    removed dist from repo, it is not needed

--- dist/lib/authenticate.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const gAuthHelper_1 = require("./gAuthHelper");
-gAuthHelper_1.GoogleAuthTestHelper.createClient().then(client => {
-    console.log("Sucessfully authenticated");
-}).catch(error => {
-    console.log("ERROR, could not authenticate with gmail", error);
-});
